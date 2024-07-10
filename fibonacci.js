a251acf65084e750d7a059c0da7573326f13b856
Patience Burani <buranipatiencepatricia@gmail.com>
2024-07-10 11:27:49 +0300

    added two fibonnaci test

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,6 @@
 function fibonacci(n) {
     // implementation will evolve through TDD cycles
+    if (n === 0) return 0;
     return 0;
   }
   
--- test/test2.js
+++ test/test2.js
@@ -5,4 +5,8 @@ describe('fibonacci', () => {
   it('should return 0 when n is 0', () => {
     assert.equal(fibonacci(0), 0);
   });
+
+  it('should return 1 when n is 1', () => {
+    assert.equal(fibonacci(1), 1);
+  });
 })
